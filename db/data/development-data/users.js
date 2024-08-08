881179f2a09f9d4bb5cc00a547fd9916b6bb8b77
dudleyspence <dudleyspence5@gmail.com>
2024-08-08 12:07:19 +0200

    adds new username URL

--- db/data/development-data/users.js
+++ db/data/development-data/users.js
@@ -3,43 +3,79 @@ module.exports = [
     username: "naturelover23",
     name: "Alice Walker",
     avatar_url:
-      "https://images.pexels.com/photos/1234567/pexels-photo-1234567.jpeg",
+      "https://st4.depositphotos.com/5532432/23354/v/450/depositphotos_233545138-stock-illustration-flat-rounded-icon-design-male.jpg",
   },
   {
     username: "gearhead99",
     name: "Bob Smith",
     avatar_url:
-      "https://images.pexels.com/photos/2345678/pexels-photo-2345678.jpeg",
+      "https://cdn1.iconfinder.com/data/icons/miscellaneous-1-5/64/Hiking-adventure-Traveler-Conservationist-avatar-512.png",
   },
   {
     username: "hikefanatic",
     name: "Charlie Johnson",
     avatar_url:
-      "https://images.pexels.com/photos/3456789/pexels-photo-3456789.jpeg",
+      "https://cdn1.iconfinder.com/data/icons/society-profession-avatar/1024/Society_Profession_Avatar_I-22-512.png",
   },
   {
     username: "outdoorenthusiast",
     name: "Dana Lee",
     avatar_url:
-      "https://images.pexels.com/photos/4567890/pexels-photo-4567890.jpeg",
+      "https://imgcdn.stablediffusionweb.com/2024/4/17/e6af22b5-5239-498b-afc5-68b71429a4f9.jpg",
   },
   {
     username: "hikeguru",
     name: "Delphine Cooper",
     avatar_url:
-      "https://images.pexels.com/photos/4567890/pexels-photo-4567790.jpeg",
+      "https://img.freepik.com/free-photo/3d-icon-travel-with-woman_23-2151037416.jpg",
   },
   {
     username: "mountainlover",
     name: "Roy McKenzie",
     avatar_url:
-      "https://images.pexels.com/photos/4567890/pexels-photo-4567290.jpeg",
+      "https://img.freepik.com/premium-vector/smiling-cartoon-hiker-with-backpack-avatar_1080480-71520.jpg?semt=ais_hybrid",
   },
 
   {
     username: "trailblazer",
     name: "Emma Rockmouth",
     avatar_url:
-      "https://images.pexels.com/photos/4567890/pexels-photo-4567894.jpeg",
+      "https://png.pngtree.com/png-vector/20230801/ourlarge/pngtree-an-avatar-for-the-boy-walking-in-the-woods-vector-png-image_6828864.png",
+  },
+  {
+    username: "tickle122",
+    name: "Tom Tickle",
+    avatar_url:
+      "https://vignette.wikia.nocookie.net/mrmen/images/d/d6/Mr-Tickle-9a.png/revision/latest?cb=20180127221953",
+  },
+  {
+    username: "grumpy19",
+    name: "Paul Grump",
+    avatar_url:
+      "https://vignette.wikia.nocookie.net/mrmen/images/7/78/Mr-Grumpy-3A.PNG/revision/latest?cb=20170707233013",
+  },
+  {
+    username: "happyamy2016",
+    name: "Amy Happy",
+    avatar_url:
+      "https://vignette1.wikia.nocookie.net/mrmen/images/7/7f/Mr_Happy.jpg/revision/latest?cb=20140102171729",
+  },
+  {
+    username: "cooljmessy",
+    name: "Peter Messy",
+    avatar_url:
+      "https://vignette.wikia.nocookie.net/mrmen/images/1/1a/MR_MESSY_4A.jpg/revision/latest/scale-to-width-down/250?cb=20170730171002",
+  },
+  {
+    username: "weegembump",
+    name: "Gemma Bump",
+    avatar_url:
+      "https://vignette.wikia.nocookie.net/mrmen/images/7/7e/MrMen-Bump.png/revision/latest?cb=20180123225553",
+  },
+  {
+    username: "jessjelly",
+    name: "Jess Jelly",
+    avatar_url:
+      "https://vignette.wikia.nocookie.net/mrmen/images/4/4f/MR_JELLY_4A.jpg/revision/latest?cb=20180104121141",
   },
 ];
